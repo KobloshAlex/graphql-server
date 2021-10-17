d09c9f924babe4a295bd2dad06e00126ae5bf39f
Alex <Koblosh.Alex@gmail.com>
2021-10-17 19:55:35 -0400

    init commint

--- app.js
+++ app.js
@@ -1,10 +1,74 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 const app = express();
 
-//app.use(bodyParser.json())
+const events = [];
 
-app.listen(3000 , () => {
-    console.log("server is up and running")
-})
+app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+    
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+    }
+    
+    type RootQuery {
+      events: [Event!]!
+    }
+    
+    type RootMutation {
+      createEvent(eventInput: EventInput): Event
+    }
+  
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {
+      events: () => events,
+      createEvent: (args) => {
+        const { description, price, title } = args.eventInput;
+        const event = {
+          _id: Math.random().toString(),
+          title: title,
+          description: description,
+          price: Number(price),
+          date: new Date().toString(),
+        };
+        events.push(event);
+
+        return event;
+      },
+    },
+
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ttjke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  )
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(3000, () => {
+  console.log("server is up and running");
+});
